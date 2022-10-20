0f3e599db07664f4179feb32018d5dfea50e528e
wickedstar <60044233+wickedstar115@users.noreply.github.com>
2022-10-20 11:18:38 -0500

    Update Buzz API Fix.user.js

--- Scripts/Buzz API Fix.user.js
+++ Scripts/Buzz API Fix.user.js
@@ -5,8 +5,7 @@
 // @description  Login fix for Buzz! (pairs well with autologin)
 // @author       wickedstar
 // @match        https://api.agilixbuzz.com/SSOProcess?domainid=27479496&url=https%3A%2F%2Fruralva.agilixbuzz.com%2Fsamlsso*
-// @match        https://wen.geniussis.com/PublicWelcome.aspx
-// @match        https://wen.geniussis.com/PublicWelcome.aspx*
+// @match        https://wen.geniussis.com/FEDashboard.aspx*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=agilixbuzz.com
 // @grant        none
 // ==/UserScript==
